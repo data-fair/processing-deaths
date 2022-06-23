2e54ada466e56b48d5f1b614f26961540a453ea4
Benjamin Giraud-Renard <bgiraud@protonmail.com>
2022-06-23 17:29:08 +0200

    fix: find date inconsistencies

--- src/process.js
+++ src/process.js
@@ -12,9 +12,9 @@ const withStreamableFile = async (filePath, fn) => {
   await fs.ensureFile(filePath + '.tmp')
   await fn(fs.createWriteStream(filePath + '.tmp'))
   // Try to prevent weird bug with NFS by forcing syncing file before reading it
-  // const fd = await fs.open(filePath + '.tmp', 'r')
-  // await fs.fsync(fd)
-  // await fs.close(fd)
+  const fd = await fs.open(filePath + '.tmp', 'r')
+  await fs.fsync(fd)
+  await fs.close(fd)
   // // write in tmp file then move it for a safer operation that doesn't create partial files
   await fs.move(filePath + '.tmp', filePath, { overwrite: true })
 }
@@ -40,32 +40,31 @@ function parseLines (lines, refCodeInseeComm, refCodeInseePays, keysRef, process
         numeroActeDeces: line.slice(167, 176).trim()
       }
       const dateN = identity.dateNaissance
-      let moisNaissance = dateN.slice(4, 6)
-      let jourNaissance = dateN.slice(6, 8)
-      if (moisNaissance === '00') {
-        moisNaissance = dateN.substr(4, 2).replace('00', '01')
-      }
-      if (jourNaissance === '00') {
-        jourNaissance = dateN.substr(4, 2).replace('00', '01')
-      }
-      identity.dateNaissance = dateN.slice(0, 4) + '-' + moisNaissance + '-' + jourNaissance
+      const anneeNaissance = dateN.slice(0, 4).padStart(4, '0')
+      const moisNaissance = dateN.slice(4, 6)
+      const jourNaissance = dateN.slice(6, 8)
+      if (anneeNaissance === '0000') identity.dateNaissance = undefined
+      else if (moisNaissance === '00') identity.dateNaissance = anneeNaissance
+      else if (jourNaissance === '00') identity.dateNaissance = anneeNaissance + '-' + moisNaissance
+      else identity.dateNaissance = anneeNaissance + '-' + moisNaissance + '-' + jourNaissance
 
       const dateM = identity.dateMort
-      let moisDeces = dateM.slice(4, 6)
-      let jourDeces = dateM.slice(6, 8)
-      if (moisDeces === '00') {
-        moisDeces = dateM.substr(4, 2).replace('00', '01')
-      }
-      if (jourDeces === '00') {
-        jourDeces = dateM.substr(4, 2).replace('00', '01')
-      }
-      identity.dateMort = dateM.slice(0, 4) + '-' + moisDeces + '-' + jourDeces
-      const age = parseFloat(dayjs(identity.dateMort).diff(dayjs(identity.dateNaissance), 'year', true).toFixed(3))
-      if (age >= 0 && age <= processingConfig.maxAge) {
-        identity.ageDeces = age
-      } else {
-        identity.ageDeces = undefined
-        identity.dateNaissance = undefined
+      const anneeDeces = dateM.slice(0, 4).padStart(4, '0')
+      const moisDeces = dateM.slice(4, 6)
+      const jourDeces = dateM.slice(6, 8)
+      if (anneeDeces === '0000') identity.dateMort = undefined
+      else if (moisDeces === '00') identity.dateMort = anneeDeces
+      else if (jourDeces === '00') identity.dateMort = anneeDeces + '-' + moisDeces
+      else identity.dateMort = anneeDeces + '-' + moisDeces + '-' + jourDeces
+
+      if (identity.dateMort && identity.dateNaissance) {
+        const age = parseFloat(dayjs(identity.dateMort).diff(dayjs(identity.dateNaissance), 'year', true).toFixed(3))
+        if (age >= 0 && age <= processingConfig.maxAge) {
+          identity.ageDeces = age
+        } else {
+          identity.ageDeces = undefined
+          identity.dateNaissance = undefined
+        }
       }
 
       if (identity.codeVilleDeces.startsWith('99')) {
@@ -96,9 +95,13 @@ function parseLines (lines, refCodeInseeComm, refCodeInseePays, keysRef, process
 
 async function updateInconsistency (tmpDir, pluginConfig, processingConfig, dataset, axios, log) {
   await log.step('Vérification des données incohérentes et demandes d\'opposition')
-
+  const currDate = dayjs().format('YYYY-MM-DD')
   const weirdAge = (await axios.get(`api/v1/datasets/${dataset.id}/lines`, { params: { qs: `ageDeces:(<0 OR >${processingConfig.maxAge})`, size: 10000 } })).data.results
+  const weirdDateNaissance = (await axios.get(`api/v1/datasets/${dataset.id}/lines`, { params: { qs: `dateNaissance:([* TO 1800-01-01] OR [${currDate} TO *])`, size: 10000 } })).data.results
+  const weirdDateMort = (await axios.get(`api/v1/datasets/${dataset.id}/lines`, { params: { qs: `dateMort:([* TO 1800-01-01] OR [${currDate} TO *])`, size: 10000 } })).data.results
   await log.info(`${weirdAge.length} ligne(s) avec un age incohérent (<0 ou >${processingConfig.maxAge})`)
+  await log.info(`${weirdDateNaissance.length} ligne(s) avec une date de naissance incohérente (<1800 ou >${currDate})`)
+  await log.info(`${weirdDateMort.length} ligne(s) avec une date de décès incohérente (<1800 ou >${currDate})`)
   const toUpdate = []
 
   const stats = {
@@ -106,15 +109,31 @@ async function updateInconsistency (tmpDir, pluginConfig, processingConfig, data
     remove: 0
   }
 
-  for (const age of weirdAge) {
-    age._action = 'update'
-    age.ageDeces = undefined
-    age.dateNaissance = undefined
-    delete age._score
-    delete age._rand
-    delete age._i
-    delete age._updatedAt
-    toUpdate.push(age)
+  const inconsistencyLine = [
+    ...weirdAge,
+    ...weirdDateMort,
+    ...weirdDateNaissance
+  ]
+
+  const result = inconsistencyLine.reduce((unique, o) => {
+    if (!unique.some(obj => obj.nom === o.nom && obj.prenom === o.prenom && obj.dateMort === o.dateMort && obj.numeroActeDeces === o.numeroActeDeces)) {
+      unique.push(o)
+    }
+    return unique
+  }, [])
+
+  await log.info(`Suppression des champs des ${result.length} lignes incohérentes`)
+
+  for (const line of result) {
+    line._action = 'update'
+    line.ageDeces = undefined
+    if (weirdDateNaissance.filter(f => line.nom === f.nom && line.prenom === f.prenom && line.numeroActeDeces === f.numeroActeDeces).length) line.dateNaissance = undefined
+    if (weirdDateMort.filter(f => line.nom === f.nom && line.prenom === f.prenom && line.numeroActeDeces === f.numeroActeDeces).length) line.dateMort = undefined
+    delete line._score
+    delete line._rand
+    delete line._i
+    delete line._updatedAt
+    toUpdate.push(line)
     stats.update++
   }
 
@@ -235,7 +254,7 @@ module.exports = async (tmpDir, refCodeInseeComm, refCodeInseePays, keysRef, plu
                   try {
                     await axios.post(`api/v1/datasets/${dataset.id}/_bulk_lines`, lines)
                   } catch (err) {
-                    await log.info(`${err.status}, ${err.statusText}`)
+                    await log.info(`${err.status}, ${err.statusText} ${err.errors.error[0]}`)
                   }
                 }
               }
